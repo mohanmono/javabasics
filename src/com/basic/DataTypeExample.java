28c69d739d07cab637c97a9d23f3b9ea1b7c44cc
mohanmomo <mohanchin789@gmail.com>
2018-05-29 15:38:46 +0530

    oh

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String[] args) {
+	
+	String firstname = "Bala";
+	String lastname  ="Singam";
+	System.out.println(firstname+lastname);
+	}
+
+}
